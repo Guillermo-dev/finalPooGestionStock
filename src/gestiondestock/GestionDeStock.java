61c11adec3d1059d08cdcf90aa8109cce2bb8150
Guillermo <guilletejera@hotmail.com>
2021-05-30 22:05:30 -0300

    cambi de detalles menores

--- src/gestiondestock/GestionDeStock.java
+++ src/gestiondestock/GestionDeStock.java
@@ -1,20 +1,10 @@
 package gestiondestock;
 
 import ui.Index;
-import hibernateUtils.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
 public class GestionDeStock {
 
     public static void main(String[] args) {
-        HibernateUtil.newSessionFactory();
-        SessionFactory sessionFactory = HibernateUtil.newSessionFactory();
-        Session session = sessionFactory.openSession();
-
-        session.close();
-        sessionFactory.close();
-
         new Index().setVisible(true);
     }
 
--- src/services/HibernateUtil.java
+++ src/services/HibernateUtil.java
@@ -1,4 +1,4 @@
-package hibernateUtils;
+package services;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
